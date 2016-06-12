49f29c1ad906b648cfdfcc92666245c5a18ccc16
Jacob Castello <jacob@arin.net>
2016-06-12 16:48:32 -0400

    scaling werth's salary for the amount of season played

--- pitchbot/plugins/werthit.py
+++ pitchbot/plugins/werthit.py
@@ -10,9 +10,29 @@ def werthit(message):
     r = http.request('GET', 'http://gd2.mlb.com/components/game/mlb/year_2016/batters/150029.xml')
     root = ET.fromstring(r.data)
     hrs = float(root.attrib['s_hr'])
-    salary = float(21000000)
+    total_salary = float(21000000)
+    games = get_standings()
+    season_pct = games / 162
+    salary = total_salary * season_pct
     locale.setlocale( locale.LC_ALL, '' )
     hrs_per = salary / hrs
     how_much = locale.currency( hrs_per, grouping=True )
     really = "Jayson Werth is being paid %s per home run this year!" % how_much
     message.reply(really)
+
+# oh god please don't look
+# ridiculous function just to get standings
+def get_standings():
+    http = urllib3.PoolManager()
+    d = http.request('GET', 'http://espn.go.com/mlb/team/_/name/wsh')
+    divs = d.data.split("div")
+    for div in divs:
+        if "mlb:teamclubhouse:standings:team\" href=\"/mlb/team/_/name/wsh" in div:
+            cols = div.split('td class="right"')
+    base_col = [z for z,x in enumerate(cols) if "Washington" in x][0]
+    windex = base_col + 1
+    lossdex = base_col + 2
+    wins = cols[windex].lstrip('>').split('<')[0]
+    losses = cols[lossdex].lstrip('>').split('<')[0]
+    total_games = int(wins) + int(losses)
+    return total_games
