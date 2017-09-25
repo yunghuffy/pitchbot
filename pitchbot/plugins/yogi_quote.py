84057fa32a6f592e3034e6bd3bf0e49bbf9d0bda
Jacob Castello <jacobcastello@gmail.com>
2017-09-24 22:19:36 -0400

    adding yogi quotes

--- pitchbot/plugins/yogi_quote.py
+++ pitchbot/plugins/yogi_quote.py
@@ -1,9 +1,13 @@
-from slackbot.bot import respond_to
+from machine.plugins.base import MachineBasePlugin
+from machine.plugins.decorators import listen_to
 from pitchbot.data.yogi_quote import quotes
 import random
 import re
 
-@respond_to('Yogi me', re.IGNORECASE)
-def yogi(message):
-    yogi_quote = quotes[random.randrange(0,49)]
-    message.reply(yogi_quote)
+
+class YogiQuote(MachineBasePlugin):
+
+    @listen_to(regex=r'(Y|y)ogi')
+    def yogi(self, message):
+        yogi_quote = quotes[random.randrange(0, 49)]
+        message.reply(yogi_quote)
