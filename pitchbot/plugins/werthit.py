0860ad20403ff546e19d36e716d42e9a72922a1e
Jacob Castello <jacobcastello@gmail.com>
2017-09-24 22:10:23 -0400

    better, but this still feels like garbage

--- pitchbot/plugins/werthit.py
+++ pitchbot/plugins/werthit.py
@@ -1,38 +1,47 @@
-from slackbot.bot import respond_to
-import re
+from datetime import datetime
+from machine.plugins.base import MachineBasePlugin
+from machine.plugins.decorators import listen_to
 import urllib3
 import xml.etree.ElementTree as ET
 import locale
+import mlbgame
+import re
 
-@respond_to('Werth', re.IGNORECASE)
-def werthit(message):
-    http = urllib3.PoolManager()
-    r = http.request('GET', 'http://gd2.mlb.com/components/game/mlb/year_2016/batters/150029.xml')
-    root = ET.fromstring(r.data)
-    hrs = float(root.attrib['s_hr'])
-    total_salary = float(21000000)
-    games = get_standings()
-    season_pct = games / 162.0
-    salary = total_salary * season_pct
-    locale.setlocale( locale.LC_ALL, '' )
-    hrs_per = salary / hrs
-    how_much = locale.currency( hrs_per, grouping=True )
-    really = "Jayson Werth is being paid %s per home run this year!" % how_much
-    message.reply(really)
 
-# oh god please don't look
-# ridiculous function just to get standings
-def get_standings():
-    http = urllib3.PoolManager()
-    d = http.request('GET', 'http://espn.go.com/mlb/team/_/name/wsh')
-    divs = d.data.split("div")
-    for div in divs:
-        if "mlb:teamclubhouse:standings:team\" href=\"/mlb/team/_/name/wsh" in div:
-            cols = div.split('td class="right"')
-    base_col = [z for z,x in enumerate(cols) if "Washington" in x][0]
-    windex = base_col + 1
-    lossdex = base_col + 2
-    wins = cols[windex].lstrip('>').split('<')[0]
-    losses = cols[lossdex].lstrip('>').split('<')[0]
-    total_games = int(wins) + int(losses)
-    return total_games
+class WerthItPlugin(MachineBasePlugin):
+
+    @listen_to(regex=r'Werth')
+    def werthit(self, msg):
+        http = urllib3.PoolManager()
+        base_url = 'http://gd2.mlb.com/components/game/mlb/year_'
+        now = datetime.now()
+        year = datetime.strftime(now, "%Y")
+        player_id = '150029'
+        player_url = base_url + year + '/batters/' + player_id + '.xml'
+        print(player_url)
+        r = http.request(
+            'GET',
+            player_url
+        )
+        root = ET.fromstring(r.data)
+        hrs = float(root.attrib['s_hr'])
+        total_salary = float(21000000)
+        games = self.get_total_games()
+        season_pct = games / 162.0
+        salary = total_salary * season_pct
+        locale.setlocale(locale.LC_ALL, '')
+        hrs_per = salary / hrs
+        how_much = locale.currency(hrs_per, grouping=True)
+        really = """
+        Jayson Werth is being paid {} per home run this year!
+        """.format(how_much)
+        msg.say(really)
+
+    def get_total_games(self):
+        all_teams = []
+        for division in mlbgame.standings().divisions:
+            all_teams.extend(division.teams)
+        for team in all_teams:
+            if team.team_abbrev == 'WSH':
+                total_games = int(team.l) + int(team.w)
+        return total_games
